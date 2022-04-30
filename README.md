a2b5ab03fc70d0cd61f433c5600b285bc4d8b991
kris <103092686+kmsedu@users.noreply.github.com>
2022-04-30 20:44:04 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple JS boilerplate
+
+As the title states, sick of having to go through this process everytime, though this is likely redundant as React is around the corner.
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css?$i/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
