62bb12e9dc6fd348966bd76d9fac021820c0f8b0
kmsedu <kmsedu@tuta.io>
2022-05-09 00:21:29 +1000

    Change API call URL to serve over HTTPS

--- src/api/fetchWeatherData.js
+++ src/api/fetchWeatherData.js
@@ -1,6 +1,6 @@
 const fetchWeatherData = async function (queryString) {
   try {
-    const geoCodePromise = await window.fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${queryString}&limit=1&appid=708eb6ce695774d5cc0dd5b02e654c48`)
+    const geoCodePromise = await window.fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${queryString}&limit=1&appid=708eb6ce695774d5cc0dd5b02e654c48`)
     const geoCode = await geoCodePromise.json()
     const dataPromise = await window.fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${geoCode[0].lat}&lon=${geoCode[0].lon}&appid=708eb6ce695774d5cc0dd5b02e654c48`)
 
